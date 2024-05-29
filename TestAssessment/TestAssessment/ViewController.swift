b7fbc36f1e7def210ea35a0d42289df2f4d564da
Athulya T Binu <athulya@tensaw.eamil>
2024-05-29 17:16:17 +0530

    New project created

---
+++ TestAssessment/TestAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAssessment
+//
+//  Created by Athulya T Binu on 29/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
