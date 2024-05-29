6b147dd4488d45a036027521ef0fa4c09c834584
Athulya T Binu <athulya@tensaw.eamil>
2024-05-29 17:20:49 +0530

    Added basic groups and API constants

---
+++ TestAssessment/TestAssessment/Constants/TAAPIConstants.swift
@@ -0,0 +1,25 @@
+//
+//  TAAPIConstants.swift
+//  TestAssessment
+//
+//  Created by Athulya T Binu on 29/05/24.
+//
+
+import Foundation
+
+struct APIConstants
+{
+    //MARK: - URLs
+    
+    static let TAAPI_baseUrl = "https://jsonplaceholder.typicode.com"
+    
+    static let TAAPI_posts = "/posts"
+    
+    //MARK: - Keys
+    
+    static let kPageKey = "page"
+    
+    static let kLimitKey = "limit"
+    
+    static let kLimitValue = 10
+}
---
+++ TestAssessment/TestAssessment/Utilities/TADataManager.swift
@@ -0,0 +1,8 @@
+//
+//  TADataManager.swift
+//  TestAssessment
+//
+//  Created by Athulya T Binu on 29/05/24.
+//
+
+import Foundation
